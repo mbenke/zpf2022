57e65fbc4a773a6296f407c59f3200106a122e16
Marcin Benke <marcin@benke.org>
2025-02-13 15:52:31 +0100

    wyklad.mk: fix mathjax

--- Slides/wyklad.mk
+++ Slides/wyklad.mk
@@ -1,9 +1,10 @@
 # Makefile borrowed from github.com/bos/stanford-cs240h
 DESTDIR=../../www
 MDFILE := $(word 1, $(basename $(wildcard *.md)))
-MATHJAXURL='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+#MATHJAXURL='http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+MATHJAXURL='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 L ?= $(MDFILE)
-MATH=--mathjax #=$(MATHJAXURL)
+MATH=--mathjax=$(MATHJAXURL)
 #MATH=--mathml
 PDOPTS=$(MATH) #--self-contained
 #PANDOC=~/.cabal/bin/pandoc $(PDOPTS)
@@ -14,7 +15,7 @@ all: $(DESTDIR)/$(L).html $(DESTDIR)/$(L)-slides.html
 
 $(DESTDIR)/$(L).html: $(L).md
 	@test -f $<
-	$(PANDOC) -s -t html --mathjax -o $@ $<
+	$(PANDOC) -s -t html -o $@ $<
 
 $(DESTDIR)/$(L)-slides.html: $(L).md Makefile # $(wildcard ./pandoc/slidy/*)
 	@test -f $<
