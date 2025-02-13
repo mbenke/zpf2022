5b37a8da958e9f5ffe7342f9743f5480457bb28d
Marcin Benke <marcin@benke.org>
2025-02-13 16:50:01 +0100

    01Typ1: FibST

---
+++ Slides/01Typ1/FibST.hs
@@ -0,0 +1,19 @@
+import Control.Monad.ST
+import Data.STRef
+fibST :: Integer -> Integer
+fibST n =
+    if n < 2 then n else runST (fib2 n)
+
+fib2 :: Integer -> ST s Integer
+fib2 n =  do
+  x <- newSTRef 0
+  y <- newSTRef 1
+  fib3 n x y
+
+fib3 0 x _ = readSTRef x
+fib3 n x y = do
+        x' <- readSTRef x
+        y' <- readSTRef y
+        writeSTRef x y'
+        writeSTRef y (x'+y')
+        fib3 (n-1) x y
