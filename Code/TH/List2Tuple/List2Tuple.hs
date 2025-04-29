e2d6039f63551c36b4d92b65f1075c275c2271b4
Marcin Benke <marcin@benke.org>
2025-04-29 13:16:56 +0200

    Code/TH: shorter List2Tuple

--- Code/TH/List2Tuple/List2Tuple.hs
+++ Code/TH/List2Tuple/List2Tuple.hs
@@ -5,7 +5,12 @@ import Language.Haskell.TH
 tuple :: Int -> ExpQ
 tuple n = [|\list -> $(tupE (exprs [|list|])) |]
   where
+{-
     exprs list = [infixE (Just (list))
                          (varE (mkName "!!"))
                          (Just (litE $ integerL (toInteger num)))
                     | num <- [0..(n - 1)]]
+-}
+    exprs :: ExpQ -> [ ExpQ ]
+    exprs list = [ [| $(list) !! num |]
+                   | num <- [0..(n - 1)]]
